2f91274aea1f81d458387326eba205a9ca640860
SudarakaKeshara <65133575+SudarakaKeshara@users.noreply.github.com>
2020-08-15 13:29:28 +0530

    Added designs for my part

---
+++ app/src/main/java/com/example/gethelp/CustomerRequestList.java
@@ -0,0 +1,14 @@
+package com.example.gethelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerRequestList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_request_list);
+    }
+}
---
+++ app/src/main/java/com/example/gethelp/ServiceRequestList.java
@@ -0,0 +1,16 @@
+package com.example.gethelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServiceRequestList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_request_list);
+
+
+    }
+}
