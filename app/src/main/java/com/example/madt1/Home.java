8494ed0fdf934175f0bd5707a3da82f9fc87dfb8
sampath <it19019314@my.sliit.lk>
2020-08-16 21:42:22 +0530

    files updated

---
+++ app/src/main/java/com/example/madt1/Home.java
@@ -0,0 +1,14 @@
+package com.example.madt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
---
+++ app/src/main/java/com/example/madt1/ProfessionalAd.java
@@ -0,0 +1,14 @@
+package com.example.madt1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessionalAd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.professional_ad);
+    }
+}
