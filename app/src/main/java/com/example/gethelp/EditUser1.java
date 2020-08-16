7964bd397e93374ddf6aae8e24128c6e76ca8121
Kalana Liyanage <it18219562@my.sliit.lk>
2020-08-16 22:30:32 +0530

    Changes with signup,login, edit user profile,popup for professional

---
+++ app/src/main/java/com/example/gethelp/EditUser1.java
@@ -0,0 +1,15 @@
+package com.example.gethelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class EditUser1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user1);
+    }
+}
