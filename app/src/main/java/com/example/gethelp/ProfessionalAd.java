171d08f4c2eaa66b642d02558d7ba47a9168b4e0
IT19163864 <it19163864@my.sliit.lk>
2020-08-17 02:39:32 +0530

    Added new dialogs for Adding and editing Service Types

---
+++ app/src/main/java/com/example/gethelp/ProfessionalAd.java
@@ -0,0 +1,14 @@
+package com.example.gethelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfessionalAd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.professional_ad);
+    }
+}
