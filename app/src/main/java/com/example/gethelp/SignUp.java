7433b7d503dd01ef43fcc441569e7cf20e5f67e0
Kalana Liyanage <it18219562@my.sliit.lk>
2020-09-24 21:30:53 +0530

    Development related to login and signup

--- app/src/main/java/com/example/gethelp/SignUp.java
+++ app/src/main/java/com/example/gethelp/SignUp.java
@@ -1,14 +1,90 @@
 package com.example.gethelp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
+    EditText Email,UserName,Age,Phone,Town,Password;
+    Button SignUpbtn,Loginbtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Email = findViewById(R.id.emailTxt);
+        UserName = findViewById(R.id.usrnmTxt);
+        Age = findViewById(R.id.ageTxt);
+        Phone = findViewById(R.id.phoneTxt);
+        Town = findViewById(R.id.adrsTxt);
+        Password = findViewById(R.id.pswrd);
+        SignUpbtn = findViewById(R.id.signUp);
+        Loginbtn = findViewById(R.id.login2);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),Home.class));
+            finish();
+        }
+
+        SignUpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = Email.getText().toString().trim();
+//                String phone = Phone.get
+                String password = Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is required");
+                    return;
+                }
+                if(password.length() < 6 ){
+                    Password.setError("Require more than 6 characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUp.this,"User Created." , Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),Home.class));
+                        }
+                        else{
+                            Toast.makeText(SignUp.this, "Error!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+        Loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),Login.class));
+            }
+        });
     }
 }
